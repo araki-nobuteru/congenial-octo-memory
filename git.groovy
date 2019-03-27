14aeea33296346ca6f1f023dbbef48baf7d9de68
Flávio Araki <araki.nobuteru@gmail.com>
2019-03-27 16:33:32 -0300

    Update git.groovy

--- git.groovy
+++ git.groovy
@@ -1,5 +1,5 @@
 def greetings(String param1, String param2) {
-    echo "greetings from git.groovy, ${param1} ${param2} ${foobaz}"
+    echo "greetings from git.groovy, ${param1} ${param2}"
 }
 
 def fx() {
