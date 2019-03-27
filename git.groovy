0f8f8e83d0eedc1298d3a68fae4da4d32d61bcf3
Flávio Araki <araki.nobuteru@gmail.com>
2019-03-27 12:48:45 -0300

    Update git.groovy

--- git.groovy
+++ git.groovy
@@ -2,4 +2,31 @@ def greetings(p) {
     echo "greetings from git.groovy"
 }
 
+def fx() {
+    stage("Stage 0") {
+        sh "echo ${currentBuild.number} > teste.txt"
+        sh "cat teste.txt"
+    }
+
+    stage("Stage 1") {
+        echo "Current build number:"
+        echo "${currentBuild.number}"
+    }
+
+    //stage('Clone repo') {
+    //    t.greetings("asdf")
+    //}    
+
+    stage('Deploy') {
+        echo "${params.asdfg}"
+    }
+
+    stage('asdf') {
+        asdf = sh returnStdout: true, script: 'pwd'
+        echo "${asdf}"
+
+        sh "cat teste.txt"
+    }
+}
+
 return this
